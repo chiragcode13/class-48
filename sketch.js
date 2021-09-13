6ae8826829f49a4b8bc936295f5200f3fcb7aedb
chiragcode13 <85103702+chiragcode13@users.noreply.github.com>
2021-09-13 16:56:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var database;
+var back_img;
+var gameState =0;
+var playerCount = 0;
+var allPlayers;
+
+var player, form,game;
+var player1,player2;
+var player1score = 0;
+var player2score = 0;
+var players;
+var fruits;
+var fruitGroup;
+var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
+var player_img;
+var gamesound,sound;
+function preload(){
+  back_img = loadImage("images/jungle.jpg");
+  player_img = loadImage("images/eagle.png");
+  fruit1_img = loadImage("images/apple2.png");
+  fruit2_img = loadImage("images/banana2.png");
+  fruit3_img = loadImage("images/melon2.png");
+  fruit4_img = loadImage("images/orange2.png");
+  fruitGroup = new Group();
+  sound = loadSound('game sound.wav');
+}
+function setup() {
+  createCanvas(1000, 600);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+ 
+  
+}
+
+function draw() {
+  background(back_img);
+  sound.loop();
+   if (playerCount === 2) {
+     game.update(1);
+       
+   }
+   if (gameState === 1) {
+     clear(); 
+     game.play();
+   }
+   if (gameState === 2) {
+    
+     game.end();
+     game.update(2);
+   }
+}
